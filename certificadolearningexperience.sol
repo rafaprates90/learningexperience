a8f8aac156adb1ef4a6d3581c84d6a065bd50969
rafaprates90 <84423386+rafaprates90@users.noreply.github.com>
2022-10-07 00:48:19 -0300

    Create certificadolearningexperience.sol

---
+++ certificadolearningexperience.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract CertificadoLearningExperience is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Certificado Learning Experience", "PKFBN") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://rafaprates90.github.io/learningexperience/";
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return "https://rafaprates90.github.io/learningexperience/metadata.json";
+    }
+}
